2f340d31fb5dbe500a0c5f8efda4b6661cba8e33
Kshitij Karthick <kshitij.karthick@gmail.com>
2020-10-29 00:57:56 +0100

    Change ml_worker to utilise config_manager and queue_manager

--- ml_worker/ml_worker/worker.py
+++ ml_worker/ml_worker/worker.py
@@ -1,29 +1,13 @@
-import os
-from resizer import predict_with_custom_resizing, build_learner
-from redis_job_queue import RedisJobQueue
-from config_manager.config_manager.redis import HOST
-from config_manager.config_manager.commons import USER_UPLOAD_PATH
-from config_manager.config_manager.job import JOB_QUEUE_NAME
-from utils import get_logger
+from queue_manager.redis_job_queue import RedisJobQueue
+from config_manager.redis import HOST
+from config_manager.job import JOB_QUEUE_NAME
+from config_manager.utils import get_logger
 
-learner = build_learner()
 logger = get_logger()
 
 
-def job(base_image_resolution, increase_resolution_percent, image_dir_name, high_res_image_name,
-        *_, **__,):
-    image_dir_path = USER_UPLOAD_PATH / image_dir_name
-    high_res_image_path = image_dir_path / high_res_image_name
-    high_res_image = predict_with_custom_resizing(
-        learner=learner, image_dir_path=image_dir_path,
-        base_image_resolution=base_image_resolution,
-        increase_resolution_percent=increase_resolution_percent,
-    )
-    high_res_image_resolution = list(high_res_image.shape)
-    high_res_image.save(high_res_image_path)
-    os.system(f'chmod -R 775 {str(image_dir_path)}')
-    del high_res_image
-    return high_res_image_resolution
+def job(*_, **__,):
+    return 'sample_result'
 
 
 def main():
