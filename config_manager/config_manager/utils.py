4e2f1b91fae3802e02c1e4aa1ef53963aea358f4
Kshitij Karthick <kshitij.karthick@gmail.com>
2020-10-29 00:57:08 +0100

    Add basic common utilities in config_manager

--- config_manager/config_manager/utils.py
+++ config_manager/config_manager/utils.py
@@ -1,13 +1,13 @@
 import logging
 
-from config_manager.config_manager.commons import LOGS_DIR_PATH
+from config_manager.commons import LOGS_DIR_PATH
 
 
-def get_logger():
+def get_logger(log_file_name='server.log'):
     logger = logging.getLogger(__name__)
     logger.setLevel(logging.DEBUG)
 
-    handler_fp = logging.FileHandler(LOGS_DIR_PATH / "worker.log")
+    handler_fp = logging.FileHandler(LOGS_DIR_PATH / log_file_name)
     handler_fp.setLevel(logging.DEBUG)
     formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
     handler_fp.setFormatter(formatter)
