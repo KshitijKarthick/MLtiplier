2de98e4acfc3fde56ce33162730bdbe72a7cc7a2
Kshitij Karthick <kshitij.karthick@gmail.com>
2020-10-29 00:56:49 +0100

    Change app_server to use config_manager appropriately

--- app_server/app_server/main.py
+++ app_server/app_server/main.py
@@ -1,25 +1,19 @@
-import os
-import tempfile
 from dataclasses import asdict
 from typing import AnyStr
 
-from PIL import Image
-
-from io import BytesIO
-from fastapi import FastAPI, File, UploadFile, HTTPException
+from fastapi import FastAPI, HTTPException
 from starlette.middleware.cors import CORSMiddleware
-from pathlib import PosixPath
 
-from redis_job_producer import RedisJobProducer
-from config_manager.config_manager.commons import USER_UPLOAD_PATH, MAX_ALLOWED_SIZE, DEBUG_MODE, \
+from queue_manager.redis_job_producer import RedisJobProducer
+from config_manager.commons import USER_UPLOAD_PATH, MAX_ALLOWED_SIZE, DEBUG_MODE, \
     HIGH_RES_IMAGE_NAME, INPUT_IMAGE_NAME
-from utils import build_logger, valid_resolution_size, obtain_enhanced_size
+from config_manager.utils import get_logger
 
-from config_manager.config_manager.job import JobStatusState
+from config_manager.job import JobStatusState
 
 app = FastAPI()
 redis_job_manager = RedisJobProducer()
-logger = build_logger()
+logger = get_logger()
 
 if DEBUG_MODE:
     app.add_middleware(CORSMiddleware, allow_origins=["*"])
@@ -30,7 +24,7 @@ async def render_status():
     return {"msg": "Ok"}
 
 
-@app.get("/api/anime/status/{job_id}")
+@app.get("/api/job/status/{job_id}")
 async def check_job_status(job_id: AnyStr):
     job_status = redis_job_manager.status(job_id=job_id)
     if job_status.status == JobStatusState.FAILURE:
@@ -39,42 +33,10 @@ async def check_job_status(job_id: AnyStr):
     return asdict(job_status)
 
 
-@app.post("/api/anime/job")
-async def anime_image_resize(file: UploadFile = File(...),
-                             increase_resolution_percent: int = 100):
+@app.post("/api/job")
+async def anime_image_resize():
     try:
-        temp_dir_path = PosixPath(tempfile.mkdtemp(dir=USER_UPLOAD_PATH))
-        temp_dir_name = str(temp_dir_path.parts[-1])
-        high_res_image_name = HIGH_RES_IMAGE_NAME
-        input_image_path = (temp_dir_path / INPUT_IMAGE_NAME)
-        _, extension = os.path.splitext(file.filename)
-        binary_img_content = await file.read()
-
-        # Expected img bytes to be uploaded
-        if not len(binary_img_content) > 0:
-            raise HTTPException(status_code=412, detail="Invalid image uploaded, no bytes "
-                                                        "obtained for the image")
-
-        img = Image.open(BytesIO(binary_img_content)).convert("RGB")
-        base_image_resolution = (img.height, img.width)
-        enhanced_img_size = obtain_enhanced_size(
-            increase_resolution_percent=increase_resolution_percent,
-            base_image_resolution=base_image_resolution)
-
-        # Disallow very large images to be predicted for memory constraints
-        if not valid_resolution_size(upscaled_size=enhanced_img_size):
-            raise HTTPException(status_code=413,
-                                detail=f"Very high resolution expected, "
-                                       f"max allowed resolution {MAX_ALLOWED_SIZE}")
-
-        img.save(input_image_path, "JPEG")
-        job_id = redis_job_manager.submit({
-            'base_image_resolution': base_image_resolution,
-            'increase_resolution_percent': increase_resolution_percent,
-            'image_dir_name': temp_dir_name,
-            'high_res_image_name': high_res_image_name,
-        })
-        del(img, file, binary_img_content)
+        job_id = redis_job_manager.submit({})
         return {
             'job_id': job_id
         }
