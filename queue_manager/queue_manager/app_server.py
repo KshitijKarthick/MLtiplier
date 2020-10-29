fa9a2131a28b9a534605d9e3cace1b302258c59e
Kshitij Karthick <kshitij.karthick@gmail.com>
2020-10-29 15:04:35 +0100

    Update the queue_manager to use the new schema

--- queue_manager/queue_manager/app_server.py
+++ queue_manager/queue_manager/app_server.py
@@ -1,6 +1,8 @@
 import json
+from typing import AnyStr, Dict
+
 from config_manager.job import JOB_QUEUE_NAME
-from config_manager.schema import Job, JobStatus, JobStatusState
+from config_manager.schema import Job, JobStatus, JobStatusState, MLWorkerInput
 from config_manager.redis import HOST
 from dataclasses import asdict
 from uuid import uuid4
@@ -8,27 +10,30 @@ from redis import Redis
 
 
 class AppServerQueueManager:
-    def __init__(self, host=HOST, job_queue_name=JOB_QUEUE_NAME):
+    def __init__(self, host: AnyStr = HOST, job_queue_name: AnyStr = JOB_QUEUE_NAME):
         self.redis = Redis(host)
         self.job_queue_name = job_queue_name
         self.job_queue_ids_name = f'{job_queue_name}_ids'
 
-    def submit(self, payload):
+    def submit(self, ml_worker_payload: MLWorkerInput):
+        if not isinstance(ml_worker_payload, MLWorkerInput):
+            raise ValueError(f'Invalid type passed, expected MLWorkerPayload,'
+                             f'received {type(ml_worker_payload)}')
         job_id = uuid4().hex
-        job = Job(**{'job_id': job_id, 'payload': payload})
+        job = Job(**{'job_id': job_id, 'payload': ml_worker_payload})
         self.redis.rpush(self.job_queue_name, self._marshall(asdict(job)))
         self.redis.rpush(self.job_queue_ids_name, job_id)
         return job_id
 
     @staticmethod
-    def _unmarshall(value):
+    def _unmarshall(value: AnyStr):
         return json.loads(value.decode('utf-8')) if value else None
 
     @staticmethod
-    def _marshall(value):
+    def _marshall(value: Dict):
         return json.dumps(value)
 
-    def status(self, job_id):
+    def status(self, job_id: AnyStr) -> JobStatus:
         # queue_length = self.redis.lindex(name=self.job_queue_ids_name, index=job_id)
         queue_length = 0
         job_status_dict = self._unmarshall(self.redis.get(job_id))
--- queue_manager/queue_manager/ml_worker.py
+++ queue_manager/queue_manager/ml_worker.py
@@ -2,14 +2,18 @@ import json
 import logging
 import time
 from dataclasses import asdict
+from typing import AnyStr, Callable, Optional
 
-from redis import Redis
-from config_manager.schema import Job, JobStatus, MLWorkerPayload, JobStatusState
-from config_manager.commons import USER_UPLOAD_PATH, INPUT_IMAGE_NAME
+from redis import Redis, RedisError, ConnectionError
+from config_manager.exception import MLPredictionException
+from config_manager.schema import Job, JobStatus, MLWorkerInput, JobStatusState, MLWorkerResult
+from config_manager.redis import HOST
+from config_manager.job import JOB_QUEUE_NAME
 
 
 class MLWorkerQueueManager:
-    def __init__(self, host, queue_name, job_fn, polling_interval=1,
+    def __init__(self, job_fn: Callable, host: AnyStr = HOST, queue_name: AnyStr = JOB_QUEUE_NAME,
+                 polling_interval=1,
                  job_status_timeout=60*60, max_retries=3, logger=logging):
         self.redis = Redis(host)
         self.queue_name = queue_name
@@ -19,9 +23,15 @@ class MLWorkerQueueManager:
         self.job_status_timeout = job_status_timeout
         self.logger = logger
 
-    def read_job(self):
+    def read_job(self) -> Optional[Job]:
         message = self.redis.lpop(self.queue_name)
-        return Job(**json.loads(message.decode('utf-8'))) if len(message) > 0 else None
+        if not message:
+            return None
+        else:
+            parsed_message = json.loads(message.decode('utf-8'))
+            print(parsed_message)
+            parsed_message['payload'] = MLWorkerInput(**parsed_message['payload'])
+            return Job(**parsed_message)
 
     def insert_job_at_end(self, job):
         self.redis.rpush(json.dumps(asdict(job)))
@@ -29,44 +39,53 @@ class MLWorkerQueueManager:
     def insert_job_at_start(self, job):
         self.redis.lpush(json.dumps(asdict(job)))
 
-    def set_job_status(self, job, job_status):
-        self.redis.setex(name=job.job_id, value=json.dumps(asdict(job_status)),
+    def set_job_status(self, job_status):
+        self.redis.setex(name=job_status.job_id, value=json.dumps(asdict(job_status)),
                          time=self.job_status_timeout)
 
-    def run(self, indefinite_wait=True):
+    def run(self, indefinite_wait=True, max_connection_errors=5, connection_error_secs=5,
+            redis_error_secs=1):
+        redis_error_count = 0
         while True:
             try:
                 job = self.read_job()
                 try:
                     if not job:
-                        if indefinite_wait:
+                        if not indefinite_wait:
                             break
                         else:
                             time.sleep(self.polling_interval)
+                            continue
                     self.logger.info(f'Running job: {job.job_id}')
-                    high_res_image_resolution = self.job_fn(**asdict(job))
+                    ml_worker_result = self.job_fn(job.payload)
                     self.logger.info(f'Job: {job.job_id} completed')
                     job_status = JobStatus(
                         job_id=job.job_id, status=JobStatusState.SUCCESS,
-                        payload=MLWorkerPayload(
-                            target=str(USER_UPLOAD_PATH / job.image_dir_name /
-                                   job.high_res_image_name),
-                            source=str(USER_UPLOAD_PATH / job.image_dir_name / INPUT_IMAGE_NAME),
-                            target_resolution=high_res_image_resolution,
-                            source_resolution=job.base_image_resolution,
-                        )
+                        result=ml_worker_result, input=job,
                     )
-                    self.set_job_status(job=job, job_status=job_status)
-                except BaseException:
+                    self.set_job_status(job_status=job_status)
+                except (MLPredictionException, BaseException) as e:
                     if job.retries < self.max_retries:
-                        self.logger.exception(f'Exception occurred: retry {job.retries}')
+                        self.logger.exception(f'Exception occurred: retry {job.retries} - {e!s}')
                         job.retries = job.retries + 1
                         self.insert_job_at_start(job=job)
                     else:
-                        self.logger.exception(f'Exception occurred retries failed')
-                        job_status = JobStatus(job_id=job.job_id, status=JobStatusState.FAILURE,
-                                               error_message='Exception occurred retries failed')
-                        self.set_job_status(job=job, job_status=job_status)
-                        self.set_job_status(job=job, job_status=False)
-            except BaseException:
-                continue
+                        error_message = f'Max number of retry failures. Job: {job.job_id}'
+                        self.logger.exception(error_message)
+                        job_status = JobStatus(
+                            job_id=job.job_id, status=JobStatusState.FAILURE,
+                            input=job, error_message=error_message
+                        )
+                        self.set_job_status(job_status=job_status)
+            except ConnectionError:
+                redis_error_count += 1
+                self.logger.exception(f'Redis connection error: {redis_error_count}')
+                time.sleep(connection_error_secs)
+                if redis_error_count > max_connection_errors:
+                    break
+            except RedisError as e:
+                self.logger.exception(f'Redis exception: {e!s}')
+                time.sleep(redis_error_secs)
+            except BaseException as e:
+                self.logger.exception(f'Fatal Exception: {e!s}')
+                raise
