e5e1a133d26ffc54466661d0175deb46096fb878
Kshitij Karthick <kshitij.karthick@gmail.com>
2020-11-02 10:46:19 +0100

    Add sapper src code

---
+++ ui/src/routes/index.svelte
@@ -0,0 +1,19 @@
+<script>
+    // Setup tutorial https://codechips.me/sapper-with-postcss-and-tailwind/
+</script>
+
+<style lang="postcss">
+  .btn {
+    @apply uppercase tracking-wide font-semibold
+      text-4xl px-4 py-3 shadow-lg rounded;
+  }
+</style>
+
+<svelte:head>
+	<title>ML Project</title>
+</svelte:head>
+
+<div class="space-y-10 text-center">
+  <h1 class="text-7xl uppercase font-bold">Great success!</h1>
+  <button class="btn">DO NOT PRESS THIS BUTTON!</button>
+</div>
